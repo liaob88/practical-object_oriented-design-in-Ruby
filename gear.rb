322451db796dadd2701826ed56cdfdca4551dbed
Ryotaro <k.ryoutaro.g@gmail.com>
2020-12-20 16:53:41 +0900

    [2.2] ギアインチを求められるように Gear クラスに gear_inches メソッドの追加

--- gear.rb
+++ gear.rb
@@ -1,15 +1,23 @@
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainring / cog.to_f
   end
+
+  def gear_inches
+    # ギアインチ＝車輪の直径*ギア比
+    # 車輪の直径＝リムの直径+タイヤの厚みの2倍
+    (rim + (tire * 2)) * ratio 
+  end
 end
 
-puts Gear.new(52, 11).ratio # => 4.7272727272727275
-puts Gear.new(30, 27).ratio # => 1.1111111111111112
+puts Gear.new(52, 11, 26, 1.5).gear_inches # => 137.0909090909091
+puts Gear.new(52, 11, 24, 1.25).gear_inches # => 125.27272727272728
